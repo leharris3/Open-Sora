45efbc7f66370a0bf376ad5301e7522aef0f7cc0
leharris3 <levlevi@email.unc.edu>
2024-08-25 08:19:19 -0400

    one more rf (:

--- scripts/train.py
+++ scripts/train.py
@@ -1,10 +1,6 @@
 import time
-import numpy as np
-import os
-import random
 import wandb
 import torch
-import math
 import warnings
 import logging
 
@@ -36,7 +32,7 @@ from opensora.models.vae import VideoAutoencoderKL
 # https://huggingface.co/hpcai-tech/OpenSora-STDiT-v3
 from opensora.models.stdit.stdit2 import STDiT2
 from opensora.utils.config_entity import TrainingConfig
-from opensora.utils.rng import save_rng_state, set_seed_custom, load_rng_state
+from opensora.utils.model_helpers import calculate_weight_norm, push_to_device
 from opensora.utils.lr_schedulers import ConstantWarmupLR, OneCycleScheduler
 from opensora.utils.wandb_logging import write_sample, log_sample
 from opensora.datasets.datasets import VariableVideoTextDataset
@@ -48,9 +44,7 @@ from opensora.acceleration.parallel_states import (
 )
 from opensora.acceleration.plugin import ZeroSeqParallelPlugin
 from opensora.datasets import (
-    prepare_dataloader,
     prepare_variable_dataloader,
-    save_sample,
 )
 from opensora.registry import DATASETS, MODELS, SCHEDULERS, build_module
 from opensora.utils.ckpt_utils import (
@@ -79,24 +73,164 @@ CAPTION_CHANNELS = 4096
 MODEL_MAX_LENGTH = 200
 
 
-def calculate_weight_norm(model):
-    total_norm = 0.0
-    for param in model.parameters():
-        param_norm = param.data.norm(2)
-        total_norm += param_norm.item() ** 2
-    total_norm = total_norm**0.5
-    return total_norm
+def train(
+    cfg,
+    coordinator,
+    logger,
+    vae,
+    model,
+    scheduler,
+    start_epoch,
+    dataloader,
+    mask_generator,
+    num_steps_per_epoch,
+    device,
+    dtype,
+    booster,
+    optimizer,
+    lr_scheduler,
+    ema,
+    writer,
+    exp_dir,
+    ema_shape_dict,
+    sampler_to_io,
+    scheduler_inference,
+):
+    for epoch in range(start_epoch, cfg.epochs):
 
+        dataloader_iter = iter(dataloader)
+        logger.info(f"Beginning epoch {epoch}...")
+        with tqdm(
+            enumerate(dataloader_iter, start=start_step),
+            desc=f"Epoch {epoch}",
+            disable=not coordinator.is_master(),
+            initial=start_step,
+            total=num_steps_per_epoch,
+        ) as pbar:
+            iteration_times = []
+            for step, batch in pbar:
+                start_time = time.time()
+                x = batch.pop("video").to(device, dtype)  # [B, C, T, H, W]
+                y = batch.pop("text")
 
-def push_to_device(item, device, dtype):
-    if isinstance(item, dict):
-        return {k: push_to_device(v, device, dtype) for k, v in item.items()}
-    elif isinstance(item, (list, tuple)):
-        return type(item)(push_to_device(v, device, dtype) for v in item)
-    elif hasattr(item, "to"):
-        return item.to(device, dtype)
-    else:
-        return item
+                # calculate visual and text encoding
+                with torch.no_grad():
+                    model_args = dict()
+                    # Prepare visual inputs
+                    x = vae.encode(x)  # [B, C, T, H/P, W/P]
+
+                # Mask
+                mask = mask_generator.get_masks(x)
+                model_args["x_mask"] = mask
+
+                # Video info and conditions
+                for k, v in batch.items():
+                    model_args[k] = push_to_device(v, device, dtype)
+
+                # Diffusion
+                t = torch.randint(
+                    0, scheduler.num_timesteps, (x.shape[0],), device=device
+                )
+                loss_dict = scheduler.training_losses(
+                    model, x, t, model_args, mask=mask
+                )
+
+                # Backward & update
+                loss = loss_dict["loss"].mean()
+                booster.backward(loss=loss, optimizer=optimizer)
+                optimizer.step()
+                optimizer.zero_grad()
+                if lr_scheduler is not None:
+                    lr_scheduler.step()
+
+                # Update EMA
+                update_ema(ema, model.module, optimizer=optimizer)
+
+                # Log loss values:
+                all_reduce_mean(loss)
+                running_loss += loss.item()
+                global_step = epoch * num_steps_per_epoch + step
+                log_step += 1
+                acc_step += 1
+                iteration_times.append(time.time() - start_time)
+
+                # log to tensorboard
+                if coordinator.is_master() and global_step % cfg.log_every == 0:
+                    avg_loss = running_loss / log_step
+                    pbar.set_postfix(
+                        {"loss": avg_loss, "step": step, "global_step": global_step}
+                    )
+                    running_loss = 0
+                    log_step = 0
+                    writer.add_scalar("loss", loss.item(), global_step)
+
+                    weight_norm = calculate_weight_norm(model)
+
+                    # TODO: log training reconstruction and trajectory.
+                    if cfg.wandb:
+                        wandb.log(
+                            {
+                                "avg_iteration_time": sum(iteration_times)
+                                / len(iteration_times),
+                                "iter": global_step,
+                                "epoch": epoch,
+                                "loss": loss.item(),
+                                "avg_loss": avg_loss,
+                                "acc_step": acc_step,
+                                "lr": optimizer.param_groups[0]["lr"],
+                                "weight_norm": weight_norm,
+                            },
+                            step=global_step,
+                        )
+                        iteration_times = []
+
+                # save checkpoint
+                if (
+                    cfg.ckpt_every > 0
+                    and global_step % cfg.ckpt_every == 0
+                    and global_step != 0
+                ):
+                    save(
+                        booster,
+                        model,
+                        ema,
+                        optimizer,
+                        lr_scheduler,
+                        epoch,
+                        step + 1,
+                        global_step + 1,
+                        cfg.batch_size,
+                        coordinator,
+                        exp_dir,
+                        ema_shape_dict,
+                        sampler=sampler_to_io,
+                    )
+                    logger.info(
+                        f"Saved checkpoint at epoch {epoch} step {step + 1} global_step {global_step + 1} to {exp_dir}"
+                    )
+
+                    # log prompts for each checkpoints
+                if global_step % cfg.eval_steps == 0:
+                    write_sample(
+                        model,
+                        vae,
+                        scheduler_inference,
+                        cfg,
+                        epoch,
+                        exp_dir,
+                        global_step,
+                        dtype,
+                        device,
+                    )
+                    log_sample(
+                        coordinator.is_master(), cfg, epoch, exp_dir, global_step
+                    )
+
+        # the continue epochs are not resumed, so we need to reset the sampler start index and start step
+        if cfg.dataset.type == "VariableVideoTextDataset":
+            dataloader.batch_sampler.set_epoch(epoch + 1)
+            print("Epoch done, recomputing batch sampler")
+        start_step = 0
 
 
 def main():
@@ -284,6 +418,7 @@ def main():
     )
 
     model_sharding(ema)
+
     # log prompts for pre-training ckpt
     first_global_step = start_epoch * num_steps_per_epoch + start_step
     write_sample(
@@ -300,147 +435,30 @@ def main():
     log_sample(coordinator.is_master(), cfg, start_epoch, exp_dir, first_global_step)
     logger.info("First global step done")
 
-    # 6.2. training loop
-    for epoch in range(start_epoch, cfg.epochs):
-        if cfg.dataset.type == "VideoTextDataset":
-            dataloader.sampler.set_epoch(epoch)
-        dataloader_iter = iter(dataloader)
-        logger.info(f"Beginning epoch {epoch}...")
-
-        with tqdm(
-            enumerate(dataloader_iter, start=start_step),
-            desc=f"Epoch {epoch}",
-            disable=not coordinator.is_master(),
-            initial=start_step,
-            total=num_steps_per_epoch,
-        ) as pbar:
-            iteration_times = []
-            for step, batch in pbar:
-                start_time = time.time()
-                x = batch.pop("video").to(device, dtype)  # [B, C, T, H, W]
-                y = batch.pop("text")
-                # Visual and text encoding
-                with torch.no_grad():
-                    model_args = dict()
-                    # Prepare visual inputs
-                    x = vae.encode(x)  # [B, C, T, H/P, W/P]
-
-                # Mask
-                if cfg.mask_ratios is not None:
-                    mask = mask_generator.get_masks(x)
-                    model_args["x_mask"] = mask
-                else:
-                    mask = None
-
-                # Video info and conditions
-                for k, v in batch.items():
-                    model_args[k] = push_to_device(v, device, dtype)
-
-                # Diffusion
-                t = torch.randint(
-                    0, scheduler.num_timesteps, (x.shape[0],), device=device
-                )
-                loss_dict = scheduler.training_losses(
-                    model, x, t, model_args, mask=mask
-                )
-
-                # Backward & update
-                loss = loss_dict["loss"].mean()
-                booster.backward(loss=loss, optimizer=optimizer)
-                optimizer.step()
-                optimizer.zero_grad()
-                if lr_scheduler is not None:
-                    lr_scheduler.step()
-
-                # Update EMA
-                update_ema(ema, model.module, optimizer=optimizer)
-
-                # Log loss values:
-                all_reduce_mean(loss)
-                running_loss += loss.item()
-                global_step = epoch * num_steps_per_epoch + step
-                log_step += 1
-                acc_step += 1
-                iteration_times.append(time.time() - start_time)
-
-                # Log to tensorboard
-                if coordinator.is_master() and global_step % cfg.log_every == 0:
-                    avg_loss = running_loss / log_step
-                    pbar.set_postfix(
-                        {"loss": avg_loss, "step": step, "global_step": global_step}
-                    )
-                    running_loss = 0
-                    log_step = 0
-                    writer.add_scalar("loss", loss.item(), global_step)
-
-                    weight_norm = calculate_weight_norm(model)
-                    # TODO: log training reconstruction and trajectory.
-                    if cfg.wandb:
-                        wandb.log(
-                            {
-                                "avg_iteration_time": sum(iteration_times)
-                                / len(iteration_times),
-                                "iter": global_step,
-                                "epoch": epoch,
-                                "loss": loss.item(),
-                                "avg_loss": avg_loss,
-                                "acc_step": acc_step,
-                                "lr": optimizer.param_groups[0]["lr"],
-                                "weight_norm": weight_norm,
-                            },
-                            step=global_step,
-                        )
-                        iteration_times = []
-
-                # Save checkpoint
-                if (
-                    cfg.ckpt_every > 0
-                    and global_step % cfg.ckpt_every == 0
-                    and global_step != 0
-                ):
-                    save(
-                        booster,
-                        model,
-                        ema,
-                        optimizer,
-                        lr_scheduler,
-                        epoch,
-                        step + 1,
-                        global_step + 1,
-                        cfg.batch_size,
-                        coordinator,
-                        exp_dir,
-                        ema_shape_dict,
-                        sampler=sampler_to_io,
-                    )
-                    logger.info(
-                        f"Saved checkpoint at epoch {epoch} step {step + 1} global_step {global_step + 1} to {exp_dir}"
-                    )
-
-                    # log prompts for each checkpoints
-                if global_step % cfg.eval_steps == 0:
-                    write_sample(
-                        model,
-                        vae,
-                        scheduler_inference,
-                        cfg,
-                        epoch,
-                        exp_dir,
-                        global_step,
-                        dtype,
-                        device,
-                    )
-                    log_sample(
-                        coordinator.is_master(), cfg, epoch, exp_dir, global_step
-                    )
-
-        # the continue epochs are not resumed, so we need to reset the sampler start index and start step
-        if cfg.dataset.type == "VideoTextDataset":
-            dataloader.sampler.set_start_index(0)
-        if cfg.dataset.type == "VariableVideoTextDataset":
-            dataloader.batch_sampler.set_epoch(epoch + 1)
-            print("Epoch done, recomputing batch sampler")
-        start_step = 0
+    # train the model
+    train(
+        cfg,
+        coordinator,
+        logger,
+        vae,
+        model,
+        scheduler,
+        start_epoch,
+        dataloader,
+        mask_generator,
+        num_steps_per_epoch,
+        device,
+        dtype,
+        booster,
+        optimizer,
+        lr_scheduler,
+        ema,
+        writer,
+        exp_dir,
+        ema_shape_dict,
+        sampler_to_io,
+        scheduler_inference,
+    )
 
 
 if __name__ == "__main__":
